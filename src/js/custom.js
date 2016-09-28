935be44c62b14dc9a56a9631d857d8a1b5c77490
Denis Kulikov <altingfest@gmail.com>
2016-09-29 01:24:15 +0300

    first commit

---
+++ src/js/custom.js
@@ -0,0 +1,27 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(document).ready(function() {
+
+    /* Custom */
+
+ /*    //= ./common/material-init.js */
+ /*    //= ./common/google-analytics.js */
+ $(".slider").owlCarousel({
+   items: 1,
+   nav: true,
+   dots: false,
+   loop: true,
+   navText: ['<div></div>','<div></div>']
+ });
+ $(".content-slider").owlCarousel({
+   items: 1,
+   nav: true,
+   dots: false,
+   loop: true,
+   navText: ['<div></div>','<div></div>']
+ });
+ 
+
+});
